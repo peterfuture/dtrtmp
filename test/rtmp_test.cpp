248ea2412e77d5860960cd2d2b0f179b67cd9cd4
peter-s <peter_future@outlook.com>
2016-06-28 18:27:46 +0800

    base commit

---
+++ test/rtmp_test.cpp
@@ -0,0 +1,22 @@
+/*
+ * =====================================================================================
+ *
+ *    Filename   :  rtmp_test.cpp
+ *    Description:  
+ *    Version    :  1.0
+ *    Created    :  2016年06月28日 18时21分49秒
+ *    Revision   :  none
+ *    Compiler   :  gcc
+ *    Author     :  peter-s
+ *    Email      :  peter_future@outlook.com
+ *    Company    :  dt
+ *
+ * =====================================================================================
+ */
+
+#include "rtmp_api.h"
+
+int main()
+{
+    return 0;
+}
