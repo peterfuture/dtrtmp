36a187a3154de23115f19c4d1a5d4674195cf41c
peter-s <peter_future@outlook.com>
2016-06-28 18:44:51 +0800

    pass Linux build

---
+++ include/rtmp_api.h
@@ -0,0 +1,18 @@
+/*
+ * =====================================================================================
+ *
+ *    Filename   :  rtmp_api.h
+ *    Description:  
+ *    Version    :  1.0
+ *    Created    :  2016年06月28日 18时41分47秒
+ *    Revision   :  none
+ *    Compiler   :  gcc
+ *    Author     :  peter-s (), peter_future@outlook.com
+ *    Company    :  dt
+ *
+ * =====================================================================================
+ */
+
+#ifndef RTMP_API_H
+
+#endif
